4fe2dae24798da312b4b23e19b6a1f124166f4b4
Thomas Gao <gaoqiang@ppznet.com>
2019-04-18 20:05:56 +0800

    20190418v1

--- 4_Python/PythonScriptAdvancedFeature.py
+++ 4_Python/PythonScriptAdvancedFeature.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
 def trim(s):
     if s:
         while s[:1] == ' ': # 切片不会出现下标越界
@@ -24,5 +25,25 @@ def findMinAndMax(s):
 
 s = ['Hello', 'World', 18, 'Apple', None]
 print([x.lower() for x in s if isinstance(x, str)])
+
+
+def triangles():
+    s = [1]
+    while True:
+        yield s
+        s = [1]+[s[i-1]+s[i] for i, v in enumerate(s) if i]+[1]
+        
+
+n = 10
+for i in triangles():
+    if n == 0:
+        break
+    n = n-1
+    print(i)
+    
+
+
+
+
 
 
