a6c8bb81430e29b27b7f7e650f3047bb7083e86a
Thomas Gao <gaoqiang@ppznet.com>
2019-05-08 21:15:06 +0800

    20190508v1

---
+++ 4_Python/PythonLogging.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import logging
+from logging.handlers import HTTPHandler
+import sys
+
+# logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.DEBUG)
+
+# StreamHandler
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_handler.setLevel(level=logging.DEBUG)
+logger.addHandler(stream_handler)
+
+# FileHandler
+file_handler = logging.FileHandler('Output.log')
+file_handler.setLevel(level=logging.INFO)
+formatter = logging.Formatter(fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+# HTTPHandler
+http_handler = HTTPHandler(host='localhost:8001', url='log', method='POST')
+logger.addHandler(http_handler)
+
+# Log
+logger.info('This is a log info')
+logger.debug('Debugging')
+logger.warning('Warning exists')
+logger.info('Finish')
